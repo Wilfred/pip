91ca0af6f9902830ce28b293855a9db6836976ab
Wilfred Hughes <me@wilfred.me.uk>
2013-08-27 18:33:50 +0100

    updating tests to take a 'script' argument

--- tests/functional/test_check.py
+++ tests/functional/test_check.py
@@ -1,38 +1,32 @@
-from tests.lib import reset_env, run_pip
-
-
-def test_check_clean():
+def test_check_clean(script):
     """On a clean environment, check shouldn't return anything.
 
     """
-    reset_env()
-    result = run_pip('check')
+    result = script.pip('check')
     assert result.stdout == ""
 
 
-def test_check_missing_dependency():
-    reset_env()
+def test_check_missing_dependency(script):
     # this will also install ipython, a dependency
-    run_pip('install', 'ipdb==0.7')
+    script.pip('install', 'ipdb==0.7')
 
     # deliberately remove the dependency
-    run_pip('uninstall', 'ipython', '--yes')
+    script.pip('uninstall', 'ipython', '--yes')
     
-    result = run_pip('check', expect_error=True, expect_stderr=True)
+    result = script.pip('check', expect_error=True, expect_stderr=True)
 
     assert result.stderr == "ipdb 0.7 requires ipython, which is not installed.\n"
     assert result.returncode == 1
 
 
-def test_check_broken_dependency():
-    reset_env()
+def test_check_broken_dependency(script):
     # this will also install a compatible version of jinja2
-    run_pip('install', 'flask==0.10.1')
+    script.pip('install', 'flask==0.10.1')
 
     # deliberately change dependency to a version that is too old
-    run_pip('install', 'jinja2==2.3')
+    script.pip('install', 'jinja2==2.3')
     
-    result = run_pip('check', expect_error=True, expect_stderr=True)
+    result = script.pip('check', expect_error=True, expect_stderr=True)
 
     assert result.stderr == "Flask 0.10.1 has requirement Jinja2>=2.4, but you have Jinja2 2.3.\n"
     assert result.returncode == 1
